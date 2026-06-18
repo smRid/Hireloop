fbff696dffe77c79bc06de1ad560ebc2c92ab31c
smRid <omorsk911@gmail.com>
2026-06-18 15:57:36 +0600

    implement Better Auth and MongoDB integration

---
+++ client/src/lib/auth.js
@@ -0,0 +1,28 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGO_DB_URI);
+const db = client.db("seekcruitr");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  user: {
+    additionalFields: {
+      role: {
+        defaultValue: "seeker",
+      },
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+});
